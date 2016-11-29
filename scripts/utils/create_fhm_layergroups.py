6bae7a0017b6a2275272cf28f6471d8fea06a153
Denise Leviste <drleviste@gmail.com>
2016-11-29 17:51:00 +0800

    Updated create_fhm_layergroups script (with error_layers log)

--- scripts/utils/create_fhm_layergroups.py
+++ scripts/utils/create_fhm_layergroups.py
@@ -7,88 +7,60 @@ import os
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'geonode.settings')
 
-from geonode.layers.models import Layer
-import copy
-import requests
+from geoserver.catalog import Catalog
 
-json_template = {
-    'layerGroup': {
-        'name': '',
-        'title': '',
-        'mode': 'SINGLE',
-        'workspace': {
-            'name': 'geonode'
-        },
-        'publishables': {
-            'published': []
-        },
-        'styles': {
-            'style': []
-        }
-    }
-}
 
-layer_json_template = {
-    '@type': 'layer',
-    # 'name': 'san_juan_fh5yr_10m',
-    'name': '',
-    # 'href': 'http:\/\/localhost\/geoserver\/rest\/layers\/san_juan_fh5yr_10m.json'
-    'href': ''
-}
+cat = Catalog(settings.OGC_SERVER['default']['LOCATION'] + 'rest',
+              username=settings.OGC_SERVER['default']['USER'],
+              password=settings.OGC_SERVER['default']['PASSWORD'])
 
-fhm_style_json = {
-    'name': 'fhm',
-    'href': 'http:\/\/localhost\/geoserver\/rest\/styles\/fhm.json'
-}
+error_layers = open('error_layers.log', 'w')
 
 for yr in ['5', '25', '100']:
 
     print('FHM ' + yr + 'yr...')
 
-    # Create json object
-    print('Creating json object...', end='')
-    layers = Layer.objects.filter(name__icontains='fh' + yr)
-    json_data = copy.deepcopy(json_template)
-
-    for layer in layers:
-        lj = copy.deepcopy(layer_json_template)
-        lj['name'] = layer.name
-        lj['href'] = 'http:\/\/localhost\/geoserver\/rest\/layers\/' + \
-            layer.name + '.json'
-        json_data['layerGroup']['publishables']['published'].append(lj)
-        json_data['layerGroup']['styles']['style'].append(fhm_style_json)
-
-    json_data['layerGroup']['name'] = 'fhms_' + yr + 'yr'
-    json_data['layerGroup']['title'] = 'Flood Hazard Maps ' + yr + 'yr'
-    # print json.dumps(json_data, indent=4, sort_keys=True)
+    # Get list of layers
+    print('Getting list of layers...', end='')
+    layers = []
+    styles = []
+    for layer in cat.get_layers():
+        try:
+            if ('fh' + yr + 'yr' in layer.name and
+                    layer.resource.enabled == 'true'):
+                # print(layer.name)
+                layers.append(layer.name)
+                if 'Merge' in layer.resource.attributes:
+                    styles.append('fhm_merge')
+                else:
+                    styles.append('fhm')
+        except:
+            print('Error fetching layer:', layer.name)
+            print(layer.name, file=error_layers)
     print('Done!')
 
+    lg_name = 'fhms_' + yr + 'yr'
+    print('lg_name:', lg_name)
+
     # Delete existing layer group if exists
-    lgs_url = 'http://localhost/geoserver/rest/workspaces/geonode/layergroups.json'
-    _auth = (settings.OGC_SERVER['default']['USER'],
-             settings.OGC_SERVER['default']['PASSWORD'])
-    r = requests.get(lgs_url, auth=_auth)
-    for lg in r.json()['layerGroups']['layerGroup']:
-        if lg['name'] == json_data['layerGroup']['name']:
-            print('Deleting existing layer group...', end='')
-            lg_url = 'http://localhost/geoserver/rest/workspaces/geonode/layergroups/' + \
-                lg['name'] + '.json'
-            r = requests.delete(lg_url, auth=_auth)
-            if r.status_code != 200:
-                print('Error deleting layer group' + lg['name'] + '! Exiting.')
-                print('r.status_code:', r.status_code)
-                print('r.text:', r.text)
-                exit(1)
+    try:
+        lg = cat.get_layergroup(name=lg_name, workspace='geonode')
+        if lg:
+            print('Deleting existing layergroup...', end='')
+            cat.delete(lg)
+            cat.reload()
             print('Done!')
-            break
+    except Exception:
+        pass
 
     # Create new layer group
     print('Creating layer group...', end='')
-    r = requests.post(lgs_url, auth=_auth, json=json_data)
-    if r.status_code != 201:
-        print('Error creating layer group! Exiting.')
-        print('r.status_code:', r.status_code)
-        print('r.text:', r.text)
-        exit(1)
+    lg = cat.create_layergroup(
+        lg_name, layers=layers, styles=styles, workspace='geonode')
+    cat.save(lg)
+    cat.reload()
     print('Done!')
+
     # break
+
+error_layers.close()
